35823a51a75d53732408039ffa092e0c3b6839f5
kerolzeeq <kerolzeeq@gmail.com>
2021-05-04 18:42:20 +0800

    Tested functions

---
+++ index.php
@@ -0,0 +1,36 @@
+    <?php
+        include 'header.php';
+        // $a = "Hello world";
+        // echo str_replace("Hello","You are",$a);
+
+        // //REPEAT STRING
+        // $fruit = "Banana";
+        // echo str_repeat($fruit,3);
+
+        // //string position
+        // $b = "Hello World!";
+        // $chars = "lo";
+        // echo stripos($b,$chars);
+
+        displayName("Hantu");
+        function displayName($name){
+            echo "My name is $name";
+        }
+    ?>
+
+    <section>
+        <h1>I AM HAZEEQ</h1>
+        <p> paragraph1 </p>
+    </section>
+
+    <footer>
+        <p>
+            Footer
+        </p>
+    </footer>
+    
+    
+
+    
+</body>
+</html>
