2da77a10a30b09fb69db474249e37acde8350a44
kerolzeeq <kerolzeeq@gmail.com>
2021-05-05 10:54:31 +0800

    Create simple-calculator

---
+++ form.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Calculator</title>
+</head>
+<body>
+    <form action="functions.php" method="get">
+        <input type="text" name="num01" placeholder="Number 1">
+        <select name="oper">
+            <label>Choose operation!</label>
+            <option value="add">Add</option>
+            <option value="sub">Subtract</option>
+            <option value="mult">Multiply</option>
+            <option value="divide">Divide</option>
+        </select>
+        <input type="text" name="num02" placeholder="Number 2">
+        <button type="submit">Calculate!</button>
+    </form>
+
+</body>
+</html>
