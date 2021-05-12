a6fc0f80895241e44e8ca182eca652bdb8d1e8c7
kerolzeeq <kerolzeeq@gmail.com>
2021-05-12 16:19:07 +0800

    Created connections.php , displaydb.php

---
+++ connections.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "fop_lecturer_es";
+
+$conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
